3e10c428eab5ad72416853a75dd8550660013428
k-metra <metrakurt@gmail.com>
2025-12-20 15:52:33 +0800

    add temporary reference to current preview's URL

--- content.js
+++ content.js
@@ -7,6 +7,9 @@ let popup = null;
 let lastTarget = null;
 let isPopupShown = false;
 
+// we store the current preview URL here
+let currentPreviewUrl = null;
+
 const ICONS = {
   folder: `<svg aria-hidden="true" focusable="false" class="octicon octicon-file-directory-fill icon-directory" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" display="inline-block" overflow="visible" style="vertical-align:text-bottom"><path d="M1.75 1A1.75 1.75 0 0 0 0 2.75v10.5C0 14.216.784 15 1.75 15h12.5A1.75 1.75 0 0 0 16 13.25v-8.5A1.75 1.75 0 0 0 14.25 3H7.5a.25.25 0 0 1-.2-.1l-.9-1.2C6.07 1.26 5.55 1 5 1H1.75Z"></path></svg>`,
   file: `<svg aria-hidden="true" focusable="false" class="octicon octicon-file color-fg-muted" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" display="inline-block" overflow="visible" style="vertical-align:text-bottom"><path d="M2 1.75C2 .784 2.784 0 3.75 0h6.586c.464 0 .909.184 1.237.513l2.914 2.914c.329.328.513.773.513 1.237v9.586A1.75 1.75 0 0 1 13.25 16h-9.5A1.75 1.75 0 0 1 2 14.25Zm1.75-.25a.25.25 0 0 0-.25.25v12.5c0 .138.112.25.25.25h9.5a.25.25 0 0 0 .25-.25V6h-2.75A1.75 1.75 0 0 1 9 4.25V1.5Zm6.75.062V4.25c0 .138.112.25.25.25h2.688l-.011-.013-2.914-2.914-.013-.011Z"></path></svg>`,
@@ -46,10 +49,12 @@ function createPopup() {
   popup.addEventListener("click", (e) => {
     e.stopPropagation();
 
-    if (lastTarget && lastTarget.href) { 
-      window.open(lastTarget.href, "_blank");
+    if (currentPreviewUrl) {
+      window.open(currentPreviewUrl, "_blank");
       destroyPopup();
-      lastTarget = null;
+
+      // clear out the current preview url
+      currentPreviewUrl = null;
     }
   });
 
@@ -200,6 +205,7 @@ function escapeHtml(text) {
 }
 
 async function handleHover(e, link) {
+  currentPreviewUrl = link.href;
   const href = link.href;
 
   if (cache.has(href)) {
