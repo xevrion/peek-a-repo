6bfcc0d3efedb3caa3137f62b715f246a3b10755
quantinium3 <quant@quantinium.dev>
2026-02-14 15:17:32 +0530

    chore: configuration file to not hardcode the client id and other vars

---
+++ config.js
@@ -0,0 +1,5 @@
+// NOTE: add the client id to oauth app
+// NOTE: add the github authorization callback url as `https://<extension_id>.chromium.org/`
+// NOTE: enable device flow
+export const GITHUB_CLIENT_ID = "";
+export const GITHUB_AUTHORIZE_URL = "https://github.com/login/oauth/authorize";
