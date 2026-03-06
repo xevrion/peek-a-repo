6528f347dcf57550d1be3961c587613a616552b6
xevrion <krbavadiya11@gmail.com>
2026-03-06 15:50:00 +0530

    chore: add back the GITHUB_CLIENT_ID to const.js

--- consts.js
+++ consts.js
@@ -1,7 +1,7 @@
 // NOTE: add the client id to oauth app
 // NOTE: add the github authorization callback url as `https://<extension_id>.chromium.org/`
 // NOTE: enable device flow
-export const GITHUB_CLIENT_ID = "";
+export const GITHUB_CLIENT_ID = "Ov23li7jLGhcwdkrnVXS";
 export const GITHUB_AUTHORIZE_URL = "https://github.com/login/oauth/authorize";
 export const GITHUB_API = "https://api.github.com/graphql";
 export const GITHUB_LOGIN_OAUTH_DEVICE_CODE_URL = "https://github.com/login/device/code"
